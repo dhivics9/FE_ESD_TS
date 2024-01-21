af2f0765a97ee102d70e121b78a54156d3fe7cc2
shalexandeer <shalexander09@gmail.com>
2024-01-21 10:10:08 +0700

    update storage.js

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,18 @@
 // get local item user as a function
-export const getUser = () => {
+const getUser = () => {
   const user = localStorage.getItem('user')
   return user ? JSON.parse(user) : null
 }
+
+// save user for remember me function
+const saveUser = (user: any) => {
+  localStorage.setItem('user', JSON.stringify(user))
+}
+
+// remove user from local storage
+const removeUser = () => {
+  localStorage.removeItem('user')
+}
+
+
+export { getUser, saveUser, removeUser }
