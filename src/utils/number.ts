41c083989315c0ff193b37412f1a42ebe3aac3dd
shalexandeer <shalexander09@gmail.com>
2024-01-22 23:52:43 +0700

    update number.js

---
+++ src/utils/number.ts
@@ -0,0 +1,8 @@
+// format date from datetime to dd/mm/yyyy
+export const formatDate = (date: Date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
